8fa6c1e170cd1e2309861c4ed338af44d684e18f
Daniel Moreano <danielmoreano@daniels-macbook-pro-3.local>
2017-12-13 12:50:30 +0100

    Result View Controller and Result Storyboard

---
+++ firstMiniChallenge/Results/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  firstMiniChallenge
+//
+//  Created by Daniel Moreano on 12/13/17.
+//  Copyright © 2017 Vincenzo Aceto. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
