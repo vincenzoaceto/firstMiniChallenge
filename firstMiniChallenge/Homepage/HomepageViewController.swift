16e7b050a8e076a72f6125dc9bfd7115705187e7
celosotomaior <30703123+celosotomaior@users.noreply.github.com>
2017-12-13 10:23:23 +0100

    Alpha Version 1 of Homepage with CollectionView.

--- firstMiniChallenge/Homepage/HomepageViewController.swift
+++ firstMiniChallenge/Homepage/HomepageViewController.swift
@@ -8,9 +8,47 @@
 
 import UIKit
 
-class HomepageViewController: UIViewController {
-
-    override func viewDidLoad() {
+class HomepageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    
+    @IBOutlet weak var TravelGuidesCollectionView: UICollectionView!
+    
+    @IBOutlet weak var CitiesAvailableCollectionView: UICollectionView!
+    
+    @IBOutlet weak var ProfileImage: UIImageView!
+    
+    var travelGuides = [#imageLiteral(resourceName: "Guide1"),#imageLiteral(resourceName: "Guide2"),#imageLiteral(resourceName: "Guide3"),#imageLiteral(resourceName: "Guide4")]
+    var citiesAvailable = [#imageLiteral(resourceName: "India"),#imageLiteral(resourceName: "brandenburger"),#imageLiteral(resourceName: "Rome"),#imageLiteral(resourceName: "Paris")]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if collectionView.tag == 2 {
+            return travelGuides.count
+        }else {
+            //            return emojis.count
+            return citiesAvailable.count
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView.tag == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CitiesIdentifier", for: indexPath) as! CitiesCollectionViewCell
+            cell.citiesImageView.image = travelGuides[indexPath.row]
+            //            cell.event = sentencesForIcons[indexPath.row]
+            
+            return cell
+            
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GuidesIdentifier", for: indexPath) as! TravelGuidesCollectionView
+            
+            cell.guidesImageView.image = citiesAvailable[indexPath.row]
+            
+            return cell
+        }
+    }
+}
+  /*  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -20,7 +58,7 @@ class HomepageViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    */
 
     /*
     // MARK: - Navigation
@@ -32,4 +70,4 @@ class HomepageViewController: UIViewController {
     }
     */
 
-}
+
