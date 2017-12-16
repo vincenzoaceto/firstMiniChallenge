0ce7a4cb4c71b9e567e558a76f0dcbf9ac63a6c7
bghelani <bghelani555@gmail.com>
2017-12-16 12:30:15 +0100

    Committing in order to be able to pull new version

--- firstMiniChallenge/Reviews/ReviewsViewController.swift
+++ firstMiniChallenge/Reviews/ReviewsViewController.swift
@@ -6,28 +6,65 @@ class ReviewsViewController: UIViewController, UITableViewDataSource, UITableVie
 
     @IBOutlet weak var tableView: UITableView!
     
-    var reviews:[String] = ["THE SOFTWARE IS PROVIDED \"AS IS\" WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE", "Review 2", "Review 3"]
+    var reviews:[String] =
+        [   "THE SOFTWARE IS PROVIDED \"AS IS\" WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. DONE."
+            , "TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+            , "Review 3"
+        ]
     var nameOfReviewers:[String] = ["Janet Ghelani", "Jane Moreano", "Amber Abdullah"]
     var ratings:[Double] = [3.6, 4.2, 4.7]
     var imageNames:[String] = ["Janet", "Jane", "Amber"]
-   
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //Properties for ReView Body
+    var fontSize = 13
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        print("counter is" + String(reviews.count))
         return reviews.count
     }
-   
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        
+        
+        
+        
+        let defaultHeightOfTheCell = 100
+        let heightForRow = (reviews[indexPath.row].count / fontSize) * 8
+        
+        //print(reviews[indexPath.row])
+        
+        
+       // print("count ; \(reviews[indexPath.row].count)")
+        //print (heightForRow)
+        
+        //return (heightForRow > defaultHeightOfTheCell) ? CGFloat(heightForRow) : CGFloat(defaultHeightOfTheCell)
+        return CGFloat(heightForRow)
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
         
+        //Properties for Name of the Reviewer
         cell.nameOfTheReviewer.text = nameOfReviewers[indexPath.row]
+        cell.nameOfTheReviewer.preferredMaxLayoutWidth = UITableViewAutomaticDimension
+        
+        //Properties for Review Body
         cell.reviewBody.text = reviews[indexPath.row]
-        cell.reviewBody.font = cell.reviewBody.font.withSize(13)
+        cell.reviewBody.font = cell.reviewBody.font.withSize(CGFloat(fontSize))
         cell.reviewBody.preferredMaxLayoutWidth = UITableViewAutomaticDimension
         cell.reviewBody.numberOfLines = 0
-        
+    
+        //Properties for Star Ratings
         cell.starRatings.rating = ratings[indexPath.row]
+        
+        //Properties  for User Image
         cell.userImage.image = UIImage(named: imageNames[indexPath.row])
+        
+        // Properties for Cell
         cell.isUserInteractionEnabled = false
         
         return cell
@@ -37,9 +74,9 @@ class ReviewsViewController: UIViewController, UITableViewDataSource, UITableVie
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 140
         
+        //tableView.rowHeight = UITableViewAutomaticDimension
+        //tableView.estimatedRowHeight = 200
         
     }
     
@@ -48,7 +85,7 @@ class ReviewsViewController: UIViewController, UITableViewDataSource, UITableVie
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
 
