208c3d5cc459e040944459e6ab9dc92f9bd0295a
Bilal Abdullah <abdullahbilal88@gmail.com>
2017-12-16 19:02:50 +0100

    Implemented Bookings view

---
+++ firstMiniChallenge/Bookings/BookingsTableViewHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookingsHeaderTableViewCell.swift
+//  firstMiniChallenge
+//
+//  Created by Bilal Abdullah on 16/12/2017.
+//  Copyright © 2017 Vincenzo Aceto. All rights reserved.
+//
+
+import UIKit
+
+class BookingsTableViewHeaderCell: UITableViewCell {
+    @IBOutlet weak var headerLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
