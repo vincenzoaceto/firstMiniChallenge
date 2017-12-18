66e1cc742b9f720f734133afd54c77763e0f4a87
Bilal Abdullah <abdullahbilal88@gmail.com>
2017-12-18 15:46:07 +0100

    Implemented DAO for Client and Agent for in memory persistance and Seed for client

---
+++ firstMiniChallenge/Dao/AgentDAO.swift
@@ -0,0 +1,42 @@
+//
+//  AgentDAO.swift
+//  firstMiniChallenge
+//
+//  Created by Bilal Abdullah on 18/12/2017.
+//  Copyright © 2017 Vincenzo Aceto. All rights reserved.
+//
+
+import Foundation
+
+class AgentDAO : DAO<Agent> {
+    static let sharedInstance = AgentDAO()
+    var agents : [Agent]
+    
+    override private init() {
+        agents = []
+        super.init()
+    }
+    
+    override func persist(_ agent: Agent) throws {
+        agents.append(agent)
+    }
+    
+    override func persist(_ agents: [Agent]) throws {
+        for agent in agents {
+            try self.persist(agent)
+        }
+    }
+    
+    override func read(_ entityId: String) -> Agent? {
+        for agent in agents {
+            if agent.entityId == entityId {
+                return agent
+            }
+        }
+        return nil
+    }
+    
+    override func readAll() -> [Agent] {
+        return agents
+    }
+}
--- firstMiniChallenge/Dao/BookingDAO.swift
+++ firstMiniChallenge/Dao/BookingDAO.swift
@@ -10,22 +10,16 @@ import Foundation
 import Firebase
 
 class BookingDAO : DAO<Booking> {
-    let tblName: String = "bookings"
-    let costName: String = "cost"
-    let dateToName: String = "dateTo"
-    let dateFromName: String = "dateFrom"
-    var ref: DatabaseReference!
+    static let sharedInstance = BookingDAO()
+    var bookings: [Booking]
     
-    override public init() {
-        ref = Database.database().reference()
+    override private init() {
+        bookings = []
         super.init()
     }
     
     override func persist(_ booking: Booking) throws {
-        let bookingRef = ref.child(tblName).child(booking.entityId)
-        bookingRef.setValue([dateFromName: booking.dateFrom.description])
-        bookingRef.setValue([dateToName: booking.dateTo.description])
-        bookingRef.setValue([costName: booking.cost])
+        bookings.append(booking)
     }
     
     override func persist(_ bookings: [Booking]) throws {
@@ -35,41 +29,15 @@ class BookingDAO : DAO<Booking> {
     }
     
     override func read(_ entityId: String) -> Booking? {
-        var booking : Booking?
-        ref.child(tblName).child(entityId).observeSingleEvent(of: .value, with: { (snapshot) in
-            let value = snapshot.value as? NSDictionary
-            let dateFrom = value?[self.dateFromName] as? String ?? ""
-            let dateTo = value?[self.dateToName] as? String ?? ""
-            let cost = value?[self.costName] as? NSDecimalNumber ?? 0.0
-            booking = Booking(self.dateFromString(dateFrom), self.dateFromString(dateTo), cost, nil, nil)
-        }) { (error) in
-            print(error.localizedDescription)
+        for booking in bookings {
+            if booking.entityId == entityId {
+                return booking
+            }
         }
-        
-        return booking
+        return nil
     }
     
     override func readAll() -> [Booking] {
-        var bookings : [Booking] = []
-        ref.child(tblName).observe(.childAdded) { snapshot in
-            for child in snapshot.children {
-                let value = snapshot.value as? NSDictionary
-                let dateFrom = value?[self.dateFromName] as? String ?? ""
-                let dateTo = value?[self.dateToName] as? String ?? ""
-                let cost = value?[self.costName] as? NSDecimalNumber ?? 0.0
-                let booking = Booking(self.dateFromString(dateFrom), self.dateFromString(dateTo), cost, nil, nil)
-                bookings.append(booking)
-            }
-        }
         return bookings
-    }
-    
-    func dateFromString(_ dateStr:String) -> Date {
-        let dateFormatter = DateFormatter()
-        guard let date = dateFormatter.date(from: dateStr) else {
-            fatalError("ERROR: Date conversion failed due to mismatched format.")
-        }
-        
-        return date
     }
 }
