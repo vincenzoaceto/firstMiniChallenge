5e59f51eef50c974d69e912f170decde00d66e1e
Unknown <marcelo.sotomaior@gmail.com>
2017-12-18 16:55:26 +0100

    Implementation of Search Bar with Segues

---
+++ firstMiniChallenge/Homepage/TableCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableCell.swift
+//  firstMiniChallenge
+//
+//  Created by Marcelo Sotomaior Azevedo on 16/12/17.
+//  Copyright © 2017 Vincenzo Aceto. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+ 
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
