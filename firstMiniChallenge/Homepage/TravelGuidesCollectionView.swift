8cd259c451c53d54839989400c0eb3ca9a72ba94
Unknown <marcelo.sotomaior@gmail.com>
2017-12-20 11:21:23 +0100

    TravelguidesCollectionView updated

--- firstMiniChallenge/Homepage/TravelGuidesCollectionView.swift
+++ firstMiniChallenge/Homepage/TravelGuidesCollectionView.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class TravelGuidesCollectionView: UICollectionViewCell {
     
+    
+    
+    var guideReviews = ["Name: Paula, 20€/day, Reviews: ","Name: Andrea, 15€/day, Reviews: ","Name: Roberta, 20€/day, Reviews: ","Name: Paulo, 15€/day, Reviews: "]
     @IBOutlet weak var travelGuidesCollectionView: UIImageView!
     
-   
+    @IBOutlet weak var guidesInfo: UILabel!
+    
+    
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        
+        return guideReviews.count
+        
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TravelGuidesIdentifier", for: indexPath) as! TravelGuidesCollectionView
+        guidesInfo.text = guideReviews[indexPath.row]
+        return cell
+    }
 }
+
+ 
+
